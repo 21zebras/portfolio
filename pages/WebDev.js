40add5a4ee242228f25eeec6a5bd3f18cf3c8658
Robert McCraw <lobozebra51@gmail.com>
2021-05-31 19:32:39 -0700

    Added responsivity to portfolio info section, column grows on mobile.

--- pages/WebDev.js
+++ pages/WebDev.js
@@ -7,27 +7,27 @@ export default function WebDev(props)
 {
     var items = [
         {
-            name: <Image  className="webPortfolio" src="/images/lobiboxfull.jpg" layout="responsive" width={1920} height={5000} alt="LobiBox" />,
+            name: <div className="TestImage"><a href="https://lobibox.com" target="_blank"><Image  className="ProjectImage" src="/images/lobibox.jpg" loading="eager" width={1834} height={5286} alt="LobiBox" /></a></div>,
             subtitle: <span>LobiBox</span>,
             description: "LobiBox is a complete lead generation and product documentation/support website, developed on Wordpress and hosted on AWS. Global elements have been templated, and the original design is 100% responsive. The graphics and mockups on the website were designed by me as well, with exception to the physical product images not placed on a rendered mockup."
         },
         {
-          name: <Image className="webPortfolio" src="/images/ruekotreats.jpg" width={1920} height={5000} alt="RuekoTreats" />,
+          name: <Image className="ProjectImage" src="/images/compressed/ruekotreats.jpg" loading="eager" width={1084} height={1329} alt="RuekoTreats" />,
           subtitle: <span className="RuekoTreats">Rueko Treats</span>,
           description: "RuekoTreats.com is a full service eCommerce website built on Wordpress with WooCommerce. Instant click-to-purchase integration has been enabled via the stripe API, and custom graphics were designed from basic product images."
         },
         {
-          name: <Image className="webPortfolio" src="/images/tmjlawncare.jpg" width={1920} height={5000} alt="TMJ Lawn Care" />,
+          name: <Image className="ProjectImage" src="/images/compressed/tmjlawncare.jpg" loading="eager" width={1084} height={1329} alt="TMJ Lawn Care" />,
           subtitle: <span className="TMJ">TMJ Lawn Care</span>,
           description: "TMJ Lawn care is a basic website designed and developed for a local business that needed to begin generating and converting leads online."
         },
         {
-          name: <Image className="webPortfolio" src="/images/jetaction.jpg" width={1920} height={5000} alt="JetAction" />,
+          name: <Image className="ProjectImage" src="/images/compressed/jetaction.jpg" loading="eager" width={1084} height={1329} alt="JetAction" />,
           subtitle: <span className="JetAction">Jet Action Rentals</span>,
           description: "Jet Action Rentals is a full-service rental and informational website for a local Jet Ski rental business. A lead-capture form and advanced analytics were installed for re-marketing to existing customers via email campaigns."
         },
         {
-          name: <Image className="webPortfolio" src="/images/sfs.jpg" width={1920} height={5000} alt="SFS" />,
+          name: <Image className="ProjectImage" src="/images/compressed/sfs.jpg" loading="eager" width={1084} height={1329} alt="SFS" />,
           subtitle: <span className="SFS">Strategies For Success AZ</span>,
           description: "Strategies for Success is a HIPAA compliant form-capture and informational website utilized by a statewide network of clinics."
         },
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Home(props)
 {
   return (
     <div className="ComingSoon">
+      <Head>
+        <title>Robert McCraw - Personal Web Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h2 className="CsHeader">Portfolio Coming Soon...</h2>
       <Link href="mailto:rmccraw51@gmail.com">Contact Robert</Link>
     </div>
