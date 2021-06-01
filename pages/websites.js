581fef1985cd1e7c2de5dbe35450dae36defd749
Robert McCraw <lobozebra51@gmail.com>
2021-06-01 00:50:27 -0700

    Added website portfolio images, white border test, profile image and links.

--- pages/websites.js
+++ pages/websites.js
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
+import { Divider } from 'rsuite'
 
-export default function Home(props)
+export default function Websites(props)
 {
   return (
     <div className="PageBody">
@@ -11,16 +13,23 @@ export default function Home(props)
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <div className="Nav">
-          <h3>Logo</h3>
-          <h3>Name</h3>
-          <h3>Email</h3>
-          <h3>Menu</h3>
-          <h3>Social</h3>
+          <Image src="/images/me-circle-is-st.png" width={512} height={512}/>
+          <h3>Robert McCraw</h3>
+          <h4>robert@mccraw.net</h4>
+          <h4>Websites</h4>
+          <h4>Ad Designs</h4>
+          <h4>Skills</h4>
+          <h4>Contact</h4>
+          <h4>Social</h4>
         </div>
         <div className="Content">
           <div className="PageTitle"><h3>Recent Websites</h3></div>
-          <div className="Col1"><h4>Test</h4></div>
-          <div className="Col2"><h4>Test</h4></div>
+          <div className="Col1"><a href="https://lobibox.com" target="_blank"><Image src="/images/square/lobibox.jpg" width={512} height={512}/></a></div>
+          <div className="Col2"><a href="https://ruekotreats.com" target="_blank"><Image src="/images/square/ruekotreats.jpg" width={512} height={512}/></a></div>
+          <div className="Col1"><a href="https://jetaction.com" target="_blank"><Image src="/images/square/jetaction.jpg" width={512} height={512}/></a></div>
+          <div className="Col2"><a href="https://strategiesforsuccessaz.com" target="_blank"><Image src="/images/square/sfs.jpg" width={512} height={512}/></a></div>
+          <div className="Col1"><a href="https://tmjlawncare.com" target="_blank"><Image src="/images/square/tmjlawncare.jpg" width={512} height={512}/></a></div>
+          <div className="Col2"><a href="https://datenightfoodreviews.com" target="_blank"><Image src="/images/square/datenighfoodreviews.jpg" width={512} height={512}/></a></div>
         </div>
       </div>
     </div>
